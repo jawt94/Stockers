7b6f30d9a56aeb6b3752218bde9955348b0b555d
Leila Gleich <email": null>
2017-02-20 14:17:01 -0500

    Move purchase price to stocks and write cost basis methods

--- db/migrate/20170220160427_create_companies.rb
+++ db/migrate/20170220160427_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :companies do |t|
       t.string :name, :ticker
-      t.integer :purchase_price, :current_price
+      t.integer :current_price
 
       t.timestamps
     end
