98103025d5b21e9e0458a41a78b196ec1a70b34a
Leila Gleich <email": null>
2017-02-20 12:28:15 -0500

    Create all controllers

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,34 @@
 class StocksController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  def destroy
+
+  end
+  
 end
