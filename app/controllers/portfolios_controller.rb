42e2ea5bf334a3267729c378f1b890b2003fccec
Leila Gleich <email": null>
2017-02-20 18:05:52 -0500

    Clean up controller code

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,10 +6,6 @@ class PortfoliosController < ApplicationController
     binding.pry
   end
 
-  def new
-    @portfolio = current_user.portfolios.new
-  end
-
   def create
     @portfolio = current_user.portfolios.new(portfolio_params)
   end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,4 +2,9 @@ module StocksHelper
   def find_stock
     @stock = Stock.find_by(id: params[:id])
   end
+
+  def validate_purchase
+    find_portfolio
+    @portfolio.cash>self.value
+  end
 end
