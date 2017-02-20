94772a54cb4bc93f7f45897be78781962fb81e3f
Leila Gleich <email": null>
2017-02-20 12:31:05 -0500

    Move shares attribute to stocks table

--- db/migrate/20170220160427_create_companies.rb
+++ db/migrate/20170220160427_create_companies.rb
@@ -2,8 +2,8 @@ class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :companies do |t|
       t.string :name, :ticker
-      t.integer :shares, :purchase_price, :current_price
-      
+      t.integer :purchase_price, :current_price
+
       t.timestamps
     end
   end
--- db/migrate/20170220161002_create_stocks.rb
+++ db/migrate/20170220161002_create_stocks.rb
@@ -3,6 +3,7 @@ class CreateStocks < ActiveRecord::Migration[5.0]
     create_table :stocks do |t|
       t.references :company, foreign_key: true
       t.references :portfolio, foreign_key: true
+      t.integer :shares, null: false
 
       t.timestamps
     end
