fad4806ca738f59584edc9e1658b2d248d6c3fde
Kendy Ng <email": null>
2017-02-21 18:18:45 -0500

    fix some parts of controller

--- app/assets/javascripts/components/Signup.es6.jsx
+++ app/assets/javascripts/components/Signup.es6.jsx
@@ -4,20 +4,22 @@ class Signup extends React.Component{
   }
 
   render() {
+      let csrfToken = $('meta[name=csrf-token]').attr('content')
     return(
     <div>
     <form className="" action="/sessions/login" method="post">
       <div className="form-group name">
-        <input className="form-control" type="email" name="name" placeholder="First Name" />
+      <input type='hidden' name='authenticity_token' value={csrfToken} />
+        <input className="form-control" type="text" name="user[name]" placeholder="Name" />
       </div>
-      <div className="form-group name">
-        <input className="form-control" type="email" name="name" placeholder="Last Name" />
+      {/* <div className="form-group name">
+        <input className="form-control" type="text" name="user[name]" placeholder="Last Name" />
+      </div> */}
+      <div className="form-group">
+        <input className="form-control" type="email" name="user[email]" placeholder="Email Address" />
       </div>
       <div className="form-group">
-        <input className="form-control" type="email" name="name" placeholder="Email Address" />
-      </div>
-      <div className="form-group">
-        <input className="form-control" type="password" name="name" placeholder="Password" />
+        <input className="form-control" type="password" name="user[password]" placeholder="Password" />
       </div>
       <button type="submit" className="btn btn-success btn-lg btn-block">Sign up</button>
     </form>
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
-      redirect_to '/portfolios'
+      redirect_to portfolios_path
     else
       @errors = ["Invalid email/password"]
       render 'sessions/login'
